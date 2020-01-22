3c72daf949b7332bf9aeff52c47f3e4bb8a1cf9d
Harrison Moore <hbmoore115@gmail.com>
2020-01-21 21:40:25 -0900

    start new file

---
+++ HW0/HW0-poem.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "A poem that I like is: Had I the heaven's embroidered cloths, Enwrought with golden and silver light, The blueand the dimand the dark cloths Of nightand lightand the half - light; I would spread the cloths under your feet : But I, being poor, have only my dreams; I have spread my dreams under your feet; Tread softly because you tread on my dreams." << std::endl;
+		return 0;
+}
