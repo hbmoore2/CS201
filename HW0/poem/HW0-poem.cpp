d0c04ca21a544224013334b43549a3347b52362f
Harrison Moore <hbmoore115@gmail.com>
2020-01-22 20:22:11 -0900

    poem done

---
+++ HW0/poem/HW0-poem.cpp
@@ -0,0 +1,24 @@
+/**
+*HW0-poem.cpp
+*Harrison Moore
+*CS201
+*Jan 22, 2020
+*/
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Here is a poem that I like:" << endl;
+	cout << "" << endl;
+	cout << "Had I the heaven's embroidered cloths," << endl;
+	cout << "Enwrought with golden and silver light," << endl;
+	cout << "The blue and the dim and the dark cloths" << endl;
+	cout << "Of night and light and the half-light;" << endl;
+	cout << "I would spread the cloths under your feet:" << endl;
+	cout << "But I, being poor, have only my dreams;" << endl;
+	cout << "I have spread my dreams under your feet;" << endl;
+	cout << "Tread softly because you tread on my dreams." << endl;
+	return 0;
+}
