387dc6814d91986f18613fa52170bf6185a299c0
Harrison Moore <hbmoore115@gmail.com>
2020-01-22 20:27:58 -0900

    game done

---
+++ HW0/game/HW0-game.cpp
@@ -0,0 +1,19 @@
+/**
+*HW0-game.cpp
+*Harrison Moore
+*CS201
+*Jan 22, 2020
+*/
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Fallout: New Vegas is a post-apocalyptic" << endl;
+	cout << "action role-playing video game. It is a spin-off" << endl;
+	cout << "of the Fallout series and was developed by" << endl;
+	cout << "Obsidian Entertainment and published by" << endl;
+	cout << "Bethesda Softworks." << endl;
+	return 0;
+}
