32efc87e7911f1d417cdd0858177a2d3bc4c2a89
Harrison Moore <hbmoore115@gmail.com>
2020-02-04 00:55:46 -0900

    miles to km converter complete.

--- HW1/mileskm/mileskm.cpp
+++ HW1/mileskm/mileskm.cpp
@@ -6,10 +6,19 @@
 //
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
 
 int main()
 {
-	cout << "meow" << endl;
+	float m, k;
+	cout << "Miles to Kilometers converter." << endl;
+	cout << endl;
+	cout << "Enter how many miles: ";
+	cin >> m;
+	cout << endl;
+
+	k = m * 1.609;
+	cout << m << " miles is " << k << " kilometers" << endl;
 	return 0;
 }
