deecd9bad9f4b6c32964cf94cf3a16725c7c1885
Harrison Moore <hbmoore115@gmail.com>
2020-02-03 23:22:09 -0900

    computes dot product and cross product of two vectors

---
+++ HW1/dotcross/dotcross.cpp
@@ -0,0 +1,49 @@
+//
+//dotcross.cpp
+//Harrison Moore
+//CS201
+//Feb 3, 2020
+//
+#include <iostream>
+#define n 3 
+using namespace std;
+
+// Function that return 
+// dot product of two vector array. 
+int dotProduct(int vect_A[], int vect_B[])
+{
+    int product = 0;
+    // Loop for calculate cot product 
+    for (int i = 0; i < n; i++)
+
+        product = product + vect_A[i] * vect_B[i];
+    return product;
+}
+// cross product of two vector
+void crossProduct(int vect_A[], int vect_B[], int cross_P[])
+{
+    cross_P[0] = vect_A[1] * vect_B[2] - vect_A[2] * vect_B[1];
+    cross_P[1] = vect_A[0] * vect_B[2] - vect_A[2] * vect_B[0];
+    cross_P[2] = vect_A[0] * vect_B[1] - vect_A[1] * vect_B[0];
+}
+// Driver function 
+int main()
+{
+    int vect_A[] = { 2, -3, 2 };
+    int vect_B[] = { 5, 4, 3 };
+    int cross_P[n];
+
+    // dotProduct function call 
+    cout << "Dot product: ";
+    cout << dotProduct(vect_A, vect_B) << endl;
+
+    // crossProduct function call 
+    cout << "Cross product: ";
+    crossProduct(vect_A, vect_B, cross_P);
+
+    // cross product of two vector
+    for (int i = 0; i < n; i++)
+
+        cout << cross_P[i] << " ";
+    return 0;
+}
