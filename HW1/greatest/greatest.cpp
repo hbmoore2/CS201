c41c9037855465b0a5158a4e2b9af12c7e376217
Harrison Moore <hbmoore115@gmail.com>
2020-02-03 22:58:51 -0900

    Need to add error messages

--- HW1/greatest/greatest.cpp
+++ HW1/greatest/greatest.cpp
@@ -6,10 +6,22 @@
 //
 #include <iostream>
 using std::cout;
+using std::cin;
 using std::endl;
 
 int main()
 {
-	cout << "ebic" endl;
+	int a,b,c,large=0;
+
+	cout << "How many numbers? (0 to end): ";
+	cin >> b;
+	for (a = 0; a < b; ++a)
+	{
+		cout << "Enter a positive integer: ";
+		cin >> c;
+		if (c > large)
+			large = c;
+	}
+	cout << "The largest number is: " << large;
 	return 0;
 }
