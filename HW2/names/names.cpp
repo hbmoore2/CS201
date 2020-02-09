978b844c7efe2c7083cb30c7b0637ce1046b0d4a
Harrison Moore <hbmoore115@gmail.com>
2020-02-08 17:09:21 -0900

    started main program

---
+++ HW2/names/names.cpp
@@ -0,0 +1,15 @@
+//
+//names.cpp
+//Harrison Moore
+//CS 201
+//Feb 10, 2020
+//
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "names start" << endl;
+	return 0;
+}
