4b3396c4a1ad944d2d13b329a10ae2806a100712
Harrison Moore <hbmoore115@gmail.com>
2020-02-10 20:59:21 -0900

    code now lists names, finds longest, and user can search for name

--- HW2/names/names.cpp
+++ HW2/names/names.cpp
@@ -9,14 +9,85 @@
 #include <string>
 #include <algorithm>
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+void InputNames(vector<string> & names);
+bool DoesNameExist(const string & nameToFind, const vector<string> names);
+void PrintNames(const vector<string> & names);
+
+
 int main(int argc, char** argv)
 {
-	std::vector<std::string> names;
-	for (int i = 0; i < 10; i++) {
-		std::string name;
-		std::cout << "Please enter a name: ";
-		std::getline(std::cin, name);
+	vector<string> names;
+	string nameToFind;
+
+	InputNames(names);
+
+	cout << "Search for a name: ";
+	cin >> nameToFind;
+	DoesNameExist(nameToFind, names);
+
+	PrintNames(names);
+
+	return 0;
+}
+
+
+void InputNames(vector<string> & names)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		string name;
+		cout << "Please enter a name: ";
+		std::getline(cin, name);
 		names.push_back(name);
 	}
-	return 0;
+	cout << "\n" << endl;
+}
+
+
+void PrintNames(const vector<string> & names)
+{
+	cout << "List of names: " << endl;
+
+	for (int i = 0; i < 10; i++)		// Gives list of names
+	{
+		cout << names[i] << endl;
+	}
+
+	// Name with most letters
+	int longest_name = 0;
+	string name;
+	cout << "\nThe name with the most letters is: " << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (names[i].size() > longest_name)
+		{
+			longest_name = names[i].size();
+			name = names[i];
+		}
+	}
+
+	cout << name;
+
+}
+
+
+bool DoesNameExist(const string & nameToFind, const vector<string> names)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (nameToFind == names[i])
+		{
+			cout << "Name found on line " << i << ": " << names[i] << "\n" << endl;
+			return true;
+		}
+	}
+	cout << "Name does not exist." << endl;
+	return false;
 }
---
+++ Practice1/prac.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+using std::vector;
+
+void InputNames(vector<string>& names);
+
+bool DoesNameExist(const string& nameToFind, const vector<string> names);
+
+void PrintNames(const vector<string>& names);
+
+
+
+int main(int argc, char** argv)
+{
+	vector<string> names;
+	string nameToFind;
+
+	InputNames(names);
+
+	cout << "Search for a name: ";
+	cin >> nameToFind;
+	DoesNameExist(nameToFind, names);
+
+	PrintNames(names);
+
+
+
+	return 0;
+}
+
+
+
+void InputNames(vector<string>& names)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		string name;
+		cout << "Please enter a name: ";
+		std::getline(cin, name);
+		names.push_back(name);
+	}
+	cout << "\n" << endl;
+}
+
+
+
+void PrintNames(const vector<string>& names)
+{
+	cout << "Names listed: " << endl;
+
+	for (int i = 0; i < 10; i++)     // Prints the list of names
+	{
+		cout << names[i] << " ";
+	}
+
+	int long_name = 0;					 // Marks the name with the most letters
+	string name;
+	cout << "\nThe name with the most letters: " << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (names[i].size() > long_name)
+		{
+			long_name = names[i].size();
+			name = names[i];
+		}
+	}
+
+	cout << name;
+
+}
+
+
+
+bool DoesNameExist(const string& nameToFind, const vector<string> names)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		if (nameToFind == names[i])
+		{
+			cout << "Name found at sector " << i << ": " << names[i] << "\n" << endl;
+			return true;
+		}
+	}
+
+	cout << "Name does not exist.\n" << endl;
+	return false;
+}
