a0e0a0f38cdae1cd5b9a2df0e4cf284cbb84dba5
Harrison Moore <hbmoore115@gmail.com>
2020-02-10 21:20:42 -0900

    input coins/money

--- HW2/money/money.cpp
+++ HW2/money/money.cpp
@@ -11,10 +11,22 @@ using std::cin;
 using std::endl;
 
 int pennies;
+int nickels;
+int dimes;
+int quarters;
+int dollars;
 
 int main()
 {
 	cout << "How many pennies do you have?" << endl;
 	cin >> pennies;
+	cout << "How many nickels do you have?" << endl;
+	cin >> nickels;
+	cout << "How many dimes do you have?" << endl;
+	cin >> dimes;
+	cout << "How many quarters do you have?" << endl;
+	cin >> quarters;
+	cout << "How many dollars do you have?" << endl;
+	cin >> dollars;
 	return 0;
 }
