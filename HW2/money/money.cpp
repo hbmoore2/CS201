441621dac084618bf8dcb5ce8c4fdb5aa6fd4b6b
Harrison Moore <hbmoore115@gmail.com>
2020-02-10 21:12:06 -0900

    started money

---
+++ HW2/money/money.cpp
@@ -0,0 +1,20 @@
+//
+//money.cpp
+//Harrison Moore
+//CS 201
+//Feb 10, 2020
+//
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int pennies;
+
+int main()
+{
+	cout << "How many pennies do you have?" << endl;
+	cin >> pennies;
+	return 0;
+}
