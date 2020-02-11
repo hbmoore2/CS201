396de26e3c4beb33953f61652fcfe9a55f34fcb7
Harrison Moore <hbmoore115@gmail.com>
2020-02-10 23:10:44 -0900

    program now returns name and score

--- HW2/scores/Scores.cpp
+++ HW2/scores/Scores.cpp
@@ -35,14 +35,23 @@ int main()
 		{
 			if (a == people[i])
 			{
-				cout << "Error: Name is already in use.";
+				cout << "Error: Name is already in use." << endl;
 				flag = true;
 			}
 		}
 		if (flag == false)
 		{
-			cout << "score: " << endl;
+			cout << "score: ";
 			cin >> b;
+
+			people.push_back(a);
+			scores.push_back(b);
+
+			cout << endl;
 		}
+	}
+	for (int i = 0; i < people.size(); i++)
+	{
+		cout << people[i] << " " << scores[i] << endl;
 	}
 }
