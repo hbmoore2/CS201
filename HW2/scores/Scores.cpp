3950f4e8161db5d5dc5af65237f06e6b8db66f2a
Harrison Moore <hbmoore115@gmail.com>
2020-02-10 23:01:10 -0900

    Input name and score

---
+++ HW2/scores/Scores.cpp
@@ -0,0 +1,48 @@
+//
+//scores.cpp
+//Harrison Moore
+//CS 201
+//Feb 10, 2020
+//
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+	vector<string> people;
+	vector<int> scores;
+
+	string a;
+	int b;
+	bool flag = false;
+
+	while (flag == false)
+	{
+		cout << "Enter a name and score. Type 'end' to stop." << endl;
+		cout << "name: ";
+		cin >> a;
+		if (a == "end")
+			break;
+
+		for (int i = 0; i < people.size(); i++)
+		{
+			if (a == people[i])
+			{
+				cout << "Error: Name is already in use.";
+				flag = true;
+			}
+		}
+		if (flag == false)
+		{
+			cout << "score: " << endl;
+			cin >> b;
+		}
+	}
+}
--- Practice1/prac.cpp
+++ Practice1/prac.cpp
@@ -1,61 +1,48 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using std::cout;
 using std::endl;
 using std::cin;
-using std::string;
 
-//
-int pennies;
-int nickels;
-int dimes;
-int quarters;
-int halfdollars;
-
-void money(int amount, string moneyplural, string money);
 int main()
 {
-	cout << "How many pennies do you have: ";
-	cin >> pennies;
+	std::vector<std::string> people; // Vectors are established
+	std::vector<int> scores;
 
-	cout << "How many nickels do you have: ";
-	cin >> nickels;
+	std::string x; //establishing variables
+	int y;
+	bool flag = false;
 
-	cout << "How many dimes do you have: ";
-	cin >> dimes;
-
-	cout << "How many quarters do you have: ";
-	cin >> quarters;
-
-	cout << "How many half dollars do you have: ";
-	cin >> halfdollars;
-
-	money(pennies, "Pennies", "Penny");
-	money(nickels, "Nickels", "Nickel");
-	money(dimes, "Dimes", "Dime");
-	money(quarters, "Quarters", "Quarter");
-	money(halfdollars, "Half Dollars", "Half Dollar");
-
-	double total = pennies * 0.01 + nickels * 0.05 + dimes * 0.10 + quarters * 0.25 + halfdollars * 0.50;
-	cout << "The total is: $" << total;
-}
-
-void money(int amount, string moneyplural, string moneysingle) // Function for money counting
-{
-	if (amount < 0)
+	while (flag == false)
 	{
-		cout << "Negative value provided" << endl;
-		return;
-	}
-	cout << "You have: " << amount << " ";
+		cout << "Input name and score, type end to stop " << endl;
+		cin >> x;
+		if (x == "end")
+			break;
 
-	if (amount == 1)
-	{
-		cout << moneysingle << endl;
-	}
-	else
-	{
-		cout << moneyplural << endl;
-	}
+		for (int i = 0; i < people.size(); i++)
+		{
+			if (x == people[i]) // error message
+			{
+				cout << "Error: Name is already in use";
+				flag = true;
+			}
+		}
+		if (flag == false)
+		{
+			cout << "score\n" << endl; // collecting score input
+			cin >> y;
 
+			people.push_back(x);
+			scores.push_back(y);
+
+			cout << "\n\n";
+		}
+	}
+	for (int i = 0; i < people.size(); i++)
+	{
+		cout << people[i] << " - " << scores[i] << "\n";// Displays ending scores and names
+	}
 }
