b1acb08bdf80c8bcbc8c557d666834671dbdd644
Harrison Moore <hbmoore115@gmail.com>
2020-02-18 22:46:00 -0900

    print added

--- HW3/boxprint.cpp
+++ HW3/boxprint.cpp
@@ -6,6 +6,7 @@
 //
 #include <iostream>
 #include <string>
+#include "boxer.h"
 using std::cout;
 using std::cin;
 using std::endl;
@@ -13,5 +14,9 @@ using std::string;
 
 int main()
 {
+	string m;
+	int box;
 
+	cout << "Enter something to print: ";
+	cin >> m;
 }
--- Practice1/prac.cpp
+++ Practice1/prac.cpp
@@ -1,26 +1,21 @@
+//
+//boxprint.cpp
+//Harrison Moore
+//CS201
+//Feb 17, 2020
+//
 #include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
 
-using namespace std;
+int main()
+{
+	string m;
+	int box;
 
-int main() {
-
-    int size;
-    string s;
-    int i;
-    cout << "Please input a string and a size integer" << endl;
-
-    cout << "Size?: " << endl;
-    cin >> size;
-    if (size > 1) {
-        cout << "String?: " << endl;
-        cin >> s;
-        for (int i = 0; s.length() > i; i++) {
-
-            for (int a = 0; a < size; a++) {
-            }
-        }
-    }
-    else {
-        cout << "Please enter a size larger than 0" << endl;
-    }
+	cout << "Enter something to print: ";
+	cin >> m;
 }
