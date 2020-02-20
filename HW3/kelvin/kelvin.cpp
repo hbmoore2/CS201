d1bcdfd9f898e8b7c60f44408aa83628bfc6e789
Harrison Moore <hbmoore115@gmail.com>
2020-02-19 22:17:18 -0900

    user input

--- HW3/kelvin/kelvin.cpp
+++ HW3/kelvin/kelvin.cpp
@@ -4,3 +4,15 @@
 //CS201
 //Feb 17, 2020
 //
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	double t = 0;
+	cout << "Enter a temperature in Celsius: ";
+	cin >> t;
+}
