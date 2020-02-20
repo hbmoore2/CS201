76efa35d32b2f923a3e58c825b08889145798a16
Harrison Moore <hbmoore115@gmail.com>
2020-02-19 21:56:28 -0900

    user input integer

--- HW3/collatz/collatz.cpp
+++ HW3/collatz/collatz.cpp
@@ -4,3 +4,15 @@
 //CS201
 //Feb 17, 2020
 //
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	int a;
+	cout << "Enter a positive integer: ";
+	cin >> a;
+}
