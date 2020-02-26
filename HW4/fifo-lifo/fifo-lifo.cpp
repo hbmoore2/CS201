88b52dadfa8a0d8ffecc757f927814d89d97b117
Harrison Moore <hbmoore115@gmail.com>
2020-02-25 22:57:36 -0900

    started fifo lifo

--- HW4/fifo-lifo/fifo-lifo.cpp
+++ HW4/fifo-lifo/fifo-lifo.cpp
@@ -3,5 +3,11 @@
 //Harrison Moore
 //CS201
 //Feb 26, 2020
-//This program will
+//This program will let a user put items into a container and take them out.
 //
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
