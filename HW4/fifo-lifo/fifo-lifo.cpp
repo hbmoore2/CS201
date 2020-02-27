e421a0855b9c37a928985a2f5075f93aa703f709
Harrison Moore <hbmoore115@gmail.com>
2020-02-26 15:59:30 -0900

    starting messages

--- HW4/fifo-lifo/fifo-lifo.cpp
+++ HW4/fifo-lifo/fifo-lifo.cpp
@@ -3,7 +3,7 @@
 //Harrison Moore
 //CS201
 //Feb 26, 2020
-//This program will let a user put items into a container and take them out.
+//This program will let a user put items into a container and pop them out.
 //
 
 #include <iostream>
@@ -11,3 +11,29 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
+using std::vector;
+
+string str;
+int fifolifo;
+
+
+int main()
+{
+	cout << "Hello, welcome to your container." << endl;
+	cout << "\nHow would you like to fill your container?" << endl;
+	cout << "With Fifo (First-In First-Out) or with Lifo (Last-In First-Out)?" << endl;
+	cout << "Enter \"Fifo\" for Fifo or \"Lifo\" for Lifo: ";
+	while (true)
+	{
+		if (fifolifo > 0) // Notifies the user of what they need to enter.
+			cout << "Please specifically enter \"Fifo\" or \"Lifo\"." << endl;
+		cin >> str;
+		if (str == "Fifo")
+			break;
+		if (str == "Lifo")
+			break;
+		fifolifo++;
+	}
+	cout << endl;
+}
--- Practice1/prac.cpp
+++ Practice1/prac.cpp
@@ -1,102 +1,11 @@
-#include <iostream>
-#include <sstream>
-#include <string>
+int Fifo_item_add_count;
+int Lifo_item_add_count;
+int yes_no = 1;
+bool b;
+int confirm_test;
+vector<string> container;
+string item;
+string answer;
+int finish = 1;
 
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
-using std::istringstream;
-
-void Bulls(string guess);
-void Cows(string guess);
-
-string ans = "7420"; // Number answer
-string guess = ""; // User's guess
-
-int main()
-{
-    cout << "!!!!!  BULLS AND COWS  !!!!!\nGuess the four digit number!" << endl;
-
-    while (guess != ans) // Loops user's input so they can keep guessing
-    {
-        getline(cin, guess);
-
-        int num;
-        istringstream error(guess); // Ensures inputs are digits
-        error >> num;
-        if (guess.size() < 4 || guess.size() > 4 || !error) // Error check
-        {
-            cout << "Please enter four digits!" << endl;
-        }
-
-        if (guess.size() == 4 && error)
-        {
-            Bulls(guess);
-            Cows(guess);
-        }
-    }
-
-    cout << "\nYou got four bulls!" << endl;
-
-    return 0;
-}
-
-///////////////BULLS FUNCTION///////////////
-void Bulls(string guess) // Searches for correct numbers in correct position
-{
-    int bulls = 0;
-    for (int i = 0; i < 4; i++) // int i = elements of answer number and guessed number
-    {
-        if (ans.at(i) == guess.at(i))
-        {
-            bulls++;
-        }
-    }
-
-    if (bulls == 0 || bulls > 1) // Outputs number of correct numbers in the correct position
-    {
-        cout << bulls << " bulls and ";
-    }
-    else if (bulls == 1)
-    {
-        cout << bulls << " bull and ";
-    }
-}
-
-
-///////////////COWS FUNCTION///////////////
-void Cows(string guess) // Searches for correct numbers in wrong position
-{
-    int cows = 0;
-
-    if (guess.at(0) == '4' || guess.at(0) == '2' || guess.at(0) == '0')
-    {
-        cows++;
-    }
-
-    if (guess.at(1) == '7' || guess.at(1) == '2' || guess.at(1) == '0')
-    {
-        cows++;
-    }
-
-    if (guess.at(2) == '7' || guess.at(2) == '4' || guess.at(2) == '0')
-    {
-        cows++;
-    }
-
-    if (guess.at(3) == '7' || guess.at(3) == '4' || guess.at(3) == '2')
-    {
-        cows++;
-    }
-
-
-    if (cows == 0 || cows > 1) // Outputs number of correct numbers in the wrong position
-    {
-        cout << cows << " cows" << endl;
-    }
-    else if (cows == 1)
-    {
-        cout << cows << " cow" << endl;
-    }
-}
+#include <vector>
