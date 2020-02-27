7fc41344dcafdb84d4b08e26b47949bdc95774d2
Harrison Moore <hbmoore115@gmail.com>
2020-02-26 15:04:32 -0900

    starting w/o vector

--- HW4/bulls-and-cows/bulls-and-cows.cpp
+++ HW4/bulls-and-cows/bulls-and-cows.cpp
@@ -8,100 +8,31 @@
 
 #include <iostream>
 #include <sstream>
-#include <vector>
 #include <string>
-#include <cstdlib>
-#include <ctime>
 
 using std::cout;
 using std::cin;
 using std::endl;
-using std::vector;
 using std::string;
 using std::istringstream;
 
+string ans = "2940";		// ANSWER
+string guess = "";			//User's guess
+
 int main()
 {
-	vector<int> Vrandom(4, -1);
-	vector<int> numbers{ 0,1,2,3,4,5,6,7,8,9 };
-
-	int guess;
-	int done = 1;
-	int bulls = 0;
-	int cows = 0;
-	int trys = 0;
-
-	string s;
-	vector<int> Vguess(4);
-
-	cout << "I am going to think of a four digit number." << endl;
-	for (int i = 0; i < 4; i++)
-	{
-		int a = -1;
-		int random;
-		while (a == -1)
-		{
-			srand(time(0));
-			random = (rand() % 10);
-			a = Vrandom[i] = numbers[random];
-			numbers[random] = -1;
-		}
-	}
-	cout << endl;
-
+	cout << "Welcome to the game of bulls and cows!" << endl;
 	cout << "Guess the four digit number: ";
-	while (bulls < 4)
+
+	while(guess != ans)
 	{
-		if (trys > 0)
-			cout << "Enter another guess: ";
-		done = 1;
-		while (done != 0)
+		getline(cin, guess);
+		int num;
+		istringstream wrong(guess);
+		wrong >> num;
+		if (guess.size() < 4 || guess.size() > 4 || !wrong)
 		{
-			std::getline(cin, s);
-			istringstream instream(s);
-			instream >> guess;
-			if (!instream)
-				cout << "Please enter integers only." << endl;
-			else
-				done = 0;
+			cout << "Enter only 4 digits!" << endl;
 		}
-		if (guess < 0)
-			for (int i = 0; i < Vrandom.size(); i++)
-			{
-				cout << Vrandom[i];
-			}
-		int num1;
-		int num2;
-		int num3;
-		int num4;
-
-		num1 = (guess / 1000);
-		Vguess[0] = (num1);
-		num2 = ((guess - num1 * 1000) / 100);
-		Vguess[1] = (num2);
-		num3 = ((guess - num1 * 1000 - num2 * 100) / 10);
-		Vguess[2] = (num3);
-		num4 = (guess - num1 * 1000 - num2 * 100 - num3 * 10);
-		Vguess[3] = (num4);
-
-		bulls = 0;
-		cows = 0;
-
-		for (int i = 0; i < Vrandom.size(); i++)
-		{
-			if (Vguess[i] == Vrandom[i])
-				bulls++;
-			else
-				for (int j = 0; j < Vrandom[i]; j++)
-					if (Vguess[j] == Vrandom[i])
-						cows++;
-		}
-		cout << endl;
 	}
-	cout << "Congratzzz! You guessed it!" << endl;
-
-	int a;
-	cout << endl;
-	cin >> a;
-	return 0;
 }
--- Practice1/prac.cpp
+++ Practice1/prac.cpp
@@ -1,37 +1,102 @@
 #include <iostream>
-#include<vector>
+#include <sstream>
+#include <string>
 
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
+using std::istringstream;
 
-int collatz(int num);
+void Bulls(string guess);
+void Cows(string guess);
+
+string ans = "7420"; // Number answer
+string guess = ""; // User's guess
+
 int main()
 {
-	int a;
-	cout << "Insert a positive number to begin: ";
-	cin >> a;
-	while (a <= 0)
-	{
-		cout << "Not a valid input, please input a POSITIVE number: ";
-		cin >> a;
-	}
-	while (a > 1)
-	{
-		cout << a << " ";
-		a = collatz(a);
-	}
-	cout << a << endl;
-	return 0;
+    cout << "!!!!!  BULLS AND COWS  !!!!!\nGuess the four digit number!" << endl;
+
+    while (guess != ans) // Loops user's input so they can keep guessing
+    {
+        getline(cin, guess);
+
+        int num;
+        istringstream error(guess); // Ensures inputs are digits
+        error >> num;
+        if (guess.size() < 4 || guess.size() > 4 || !error) // Error check
+        {
+            cout << "Please enter four digits!" << endl;
+        }
+
+        if (guess.size() == 4 && error)
+        {
+            Bulls(guess);
+            Cows(guess);
+        }
+    }
+
+    cout << "\nYou got four bulls!" << endl;
+
+    return 0;
 }
-int collatz(int num)
+
+///////////////BULLS FUNCTION///////////////
+void Bulls(string guess) // Searches for correct numbers in correct position
 {
-	if (num % 2 == 0)
-	{
-		return num / 2;
-	}
-	else
-	{
-		return num * 3 + 1;
-	}
+    int bulls = 0;
+    for (int i = 0; i < 4; i++) // int i = elements of answer number and guessed number
+    {
+        if (ans.at(i) == guess.at(i))
+        {
+            bulls++;
+        }
+    }
+
+    if (bulls == 0 || bulls > 1) // Outputs number of correct numbers in the correct position
+    {
+        cout << bulls << " bulls and ";
+    }
+    else if (bulls == 1)
+    {
+        cout << bulls << " bull and ";
+    }
+}
+
+
+///////////////COWS FUNCTION///////////////
+void Cows(string guess) // Searches for correct numbers in wrong position
+{
+    int cows = 0;
+
+    if (guess.at(0) == '4' || guess.at(0) == '2' || guess.at(0) == '0')
+    {
+        cows++;
+    }
+
+    if (guess.at(1) == '7' || guess.at(1) == '2' || guess.at(1) == '0')
+    {
+        cows++;
+    }
+
+    if (guess.at(2) == '7' || guess.at(2) == '4' || guess.at(2) == '0')
+    {
+        cows++;
+    }
+
+    if (guess.at(3) == '7' || guess.at(3) == '4' || guess.at(3) == '2')
+    {
+        cows++;
+    }
+
+
+    if (cows == 0 || cows > 1) // Outputs number of correct numbers in the wrong position
+    {
+        cout << cows << " cows" << endl;
+    }
+    else if (cows == 1)
+    {
+        cout << cows << " cow" << endl;
+    }
 }
