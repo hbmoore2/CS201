cc6a93991a3e0d80ab095db0cc1510b612b6fb92
Harrison Moore <hbmoore115@gmail.com>
2020-02-26 16:44:52 -0900

    void fifopush added to program

--- HW4/fifo-lifo/fifo-lifo.cpp
+++ HW4/fifo-lifo/fifo-lifo.cpp
@@ -16,12 +16,15 @@ using std::string;
 using std::vector;
 
 string str;
+string item;
+string answer;
+vector<string> container;
 int fifolifo;
 int fifo;
 int lifo;
 int yes_no = 1;
-string item;
-vector<string> container;
+int finish = 1;
+bool b;
 
 // First-In First-Out
 void FifoPush(vector<string>& container, const string& item);
@@ -41,96 +44,155 @@ bool TestLifo();
 
 int main()
 {
-	cout << "Hello, welcome to your container." << endl;
-	cout << "\nHow would you like to fill your container?" << endl;
-	cout << "With Fifo (First-In First-Out) or with Lifo (Last-In First-Out)?" << endl;
-	cout << "Enter \"Fifo\" for Fifo or \"Lifo\" for Lifo: ";
-	while (true)
+	if (finish == 0)
 	{
-		if (fifolifo > 0) // Notifies the user of what they need to enter.
-			cout << "Please enter \"Fifo\" or \"Lifo\"." << endl;
-		cin >> str;
-		if (str == "Fifo")
-			break;
-		if (str == "Lifo")
-			break;
-		fifolifo++;
-	}
-	cout << endl;
-
-	if (str == "Fifo")		//Fifo picked
-	{
-		cout << "You chose Fifo. Enter something to place in your container: ";
-		yes_no = 1;
-		while (yes_no != 0)
+		cout << "Hello, welcome to your container." << endl;
+		cout << "\nHow would you like to fill your container?" << endl;
+		cout << "With Fifo (First-In First-Out) or with Lifo (Last-In First-Out)?" << endl;
+		cout << "Enter \"Fifo\" for Fifo or \"Lifo\" for Lifo: ";
+		while (true)
 		{
-			if (fifo > 0)
-				cout << "Enter another item to place in your container: ";
-
-			cin >> item;
-			FifoPush(container, item);
-
-			cout << "Would you like to add another item? Enter \"1\" for yes or \"0\" for no: ";
-			cin >> yes_no;
-
-			fifo++;
+			if (fifolifo > 0) // Notifies the user of what they need to enter.
+				cout << "Please enter \"Fifo\" or \"Lifo\"." << endl;
+			cin >> str;
+			if (str == "Fifo")
+				break;
+			if (str == "Lifo")
+				break;
+			fifolifo++;
 		}
+		cout << endl;
 
-		yes_no = 1;
-		while (yes_no != 0)
+		if (str == "Fifo")		//Fifo picked
 		{
-			cout << endl;
-			cout << "Would you like to \"Pop\" the last item out of your container? Enter \"1\" for yes or \"0\" for no: ";
-			cin >> yes_no;
-			if (yes_no == 1)
+			cout << "You chose Fifo. Enter something to place in your container: ";
+			yes_no = 1;
+			while (yes_no != 0)
 			{
-				if (container.size() == 0)
-				{
-					cout << "Your container is empty.";
-					break;
-				}
-				cout << "Your last item was: ";
-				FifoPop(container, item);
-			}
-		}
-	}
+				if (fifo > 0)
+					cout << "Enter another item to place in your container: ";
 
-	else if (str == "Lifo") // picked lifo
-	{
-		cout << "You chose Lifo. Enter something to place in your container: ";
-		yes_no = 1;
-		while (yes_no != 0)
-		{
-			if (lifo > 0)
-				cout << "Enter another item to place in your container: ";
 				cin >> item;
-			LifoPush(container, item);
-			cout << "Would you like to add another item? Enter \"1\" for yes or \"0\" for no: ";
-			cin >> yes_no;
-			lifo++;
+				FifoPush(container, item);
+
+				cout << "Would you like to add another item? Enter \"1\" for yes or \"0\" for no: ";
+				cin >> yes_no;
+
+				fifo++;
+			}
+
+			yes_no = 1;
+			while (yes_no != 0)
+			{
+				cout << endl;
+				cout << "Would you like to \"Pop\" the last item out of your container? Enter \"1\" for yes or \"0\" for no: ";
+				cin >> yes_no;
+				if (yes_no == 1)
+				{
+					if (container.size() == 0)
+					{
+						cout << "Your container is empty.";
+						break;
+					}
+					cout << "Your last item was: ";
+					FifoPop(container, item);
+				}
+			}
 		}
 
-		yes_no = 1;
-		while (yes_no != 0)
+		else if (str == "Lifo") // picked lifo
 		{
-			cout << endl;
-			cout << "Would you like to \"Pop\" the last item out of your container? Enter \"1\" for yes or \"0\" for no: ";
-			cin >> yes_no;
-			if (yes_no == 1)
+			cout << "You chose Lifo. Enter something to place in your container: ";
+			yes_no = 1;
+			while (yes_no != 0)
 			{
-				if (container.size() == 0)
+				if (lifo > 0)
+					cout << "Enter another item to place in your container: ";
+				cin >> item;
+				LifoPush(container, item);
+				cout << "Would you like to add another item? Enter \"1\" for yes or \"0\" for no: ";
+				cin >> yes_no;
+				lifo++;
+			}
+
+			yes_no = 1;
+			while (yes_no != 0)
+			{
+				cout << endl;
+				cout << "Would you like to \"Pop\" the last item out of your container? Enter \"1\" for yes or \"0\" for no: ";
+				cin >> yes_no;
+				if (yes_no == 1)
 				{
-					cout << "Your container is empty.";
-					break;
+					if (container.size() == 0)
+					{
+						cout << "Your container is empty.";
+						break;
+					}
+					cout << "Your last item was: ";
+					LifoPop(container, item);
 				}
-				cout << "Your last item was: ";
-				LifoPop(container, item);
+			}
+		}
+		finish = 3; // items in container
+		cout << "Do want to know what is in your container? Type: yes or no: ";
+		while (finish != 0)
+		{
+			cin >> answer;
+			if (answer == "yes")
+				finish = 0;
+			else if (answer == "Yes")
+				finish = 0;
+			else if (answer == "YES")
+				finish = 0;
+			else if (answer == "no")
+			{
+				finish = 1;
+				break;
+			}
+			else if (answer == "No")
+			{
+				finish = 1;
+				break;
+			}
+			else if (answer == "NO")
+			{
+				finish = 1;
+				break;
+			}
+			else
+				cout << "Please type: yes or no: ";
+
+			if (finish == 0) // Checks container
+			{
+				b = IsContainerEmpty(container);
+				if (b)
+					cout << "Your container is empty." << endl;
+				else
+				{
+					cout << "These are the items in your container: " << endl;
+					PrintContainer(container);
+				}
+				break;
 			}
 		}
 	}
-	finish = 3; // items in container
-	cout << "Do want to know what is in your container? Type: yes or no: ";
-	while (finish != 0)
+
+
+
+	// First-In First-Out
+	void FifoPush(vector<string>& container, const string& item) //puts item in front
 	{
-		cin >> answer;
+		int container_size = container.size();
+		if (container_size > 0)
+		{
+			container.resize(container_size + 1);
+			for (int i = (container_size); i > 0; i--)
+			{
+				container[i] = container[i - 1];
+			}
+			container[0] = item;
+		}
+		else
+			container.push_back(item);
+	}
 }
