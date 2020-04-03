2b71c8bf72b1b3427807aa664a0426d5f57d2a24
Harrison Moore <hbmoore115@gmail.com>
2020-04-03 11:25:23 -0800

    added button for truncate

--- HW5/fltk-truc.cpp
+++ HW5/fltk-truc.cpp
@@ -14,6 +14,8 @@
 #include <FL/Fl_Window.h>
 #include <FL/Fl_Input.h>
 #include <FL/Fl_Output.h>
+#include <FL/Fl_Value_Input.h>
+#include <FL/Fl_Button.h>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -30,7 +32,7 @@ int main(int argc, char** argv)
     box1->labelsize(16);
 
 //Instructions
-    Fl_Box* box2 = new Fl_Box(60, 60, 400, 20, "To truncate, you need to input a string and an integer value.");
+    Fl_Box* box2 = new Fl_Box(60, 60, 400, 20, "Enter a string to truncate.");
     box2->box(FL_UP_BOX);
     box2->labelfont(FL_ITALIC);
     box2->labelsize(12);
@@ -41,4 +43,22 @@ int main(int argc, char** argv)
     input1->labelfont(FL_ITALIC);
     input1->labelsize(12);
     input1->value("");
+
+ //User input (Integer)
+    Fl_Box* box3 = new Fl_Box(60, 120, 300, 20, "Enter the number of characters you want to truncate.");
+    box3->box(FL_UP_BOX);
+    box3->labelfont(FL_ITALIC);
+    box3->labelsize(12);
+    Fl_Value_Input* input2 = new Fl_Value_Input(150, 150, 130, 20, "Pick an integer:");
+    input2->box(FL_UP_BOX);
+    input2->labelfont(FL_ITALIC);
+    input2->labelsize(12);
+    input2->value(integer);
+
+ //Truncate button
+    Fl_Button* button1 = new Fl_Button(130, 180, 100, 20, "Truncate");
+    button1->box(FL_UP_BOX);
+    button1->labelfont(FL_BOLD + FL_ITALIC);
+    button1->labelsize(12);
+    button1->value();
 }
