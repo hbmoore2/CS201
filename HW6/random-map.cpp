4eaf9210032affc4e07342a29362a81f0a9029a3
Harrison Moore <hbmoore115@gmail.com>
2020-04-10 19:07:01 -0800

    included main and copied the code from hw guidelines

--- HW6/random-map.cpp
+++ HW6/random-map.cpp
@@ -40,3 +40,31 @@ void PrintDistribution(
     const map<int, int>& numbers,
     const int& star);
 
+int main()
+{
+    // Seed with a real random value, if available
+    random_device r;
+
+    std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
+    mt19937 e2(seed2);
+
+
+    int mean = (first + last) / 2;
+    int star = (1000 / (last - first));
+
+    map<int, int> hist1;
+    for (int n = 0; n < 10000; ++n)
+    {
+        ++hist1[std::round(RandomBetweenU(first, last, r))];
+    }
+    map<int, int> hist2;
+    for (int n = 0; n < 10000; ++n)
+    {
+        ++hist2[std::round(1 + RandomBetweenN(first, last, mean, e2))];
+    }
+    map<int, int> hist3;
+    for (int n = 0; n < 10000; ++n)
+    {
+        ++hist3[std::round(RandomBetween(first, last))];
+    }
+}
