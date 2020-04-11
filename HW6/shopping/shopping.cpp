381e63689b92f9fbcf8e9ce53491725714e3a880
Harrison Moore <hbmoore115@gmail.com>
2020-04-10 22:26:34 -0800

    added the inventory for shopping.cpp

--- HW6/shopping/shopping.cpp
+++ HW6/shopping/shopping.cpp
@@ -3,5 +3,36 @@
 //Harrison Moore
 //CS201
 //April 10, 2020
-//This program will...
 //
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include <algorithm>
+using std::cout;
+using std::endl;
+using std::cin;
+struct Record
+{
+	double unitPrice;
+	int units;
+};
+
+void printInventory(std::map<std::string, Record>& inventory);
+void buy(std::map<std::string, Record>& inventory, double& total);
+void drop(std::map<std::string, Record>& inventory, double& total);
+
+int main()
+{
+	std::string action;
+	std::map<std::string, Record > inventory;
+	inventory["Napkin"] = { 5.0,5 };
+	inventory["toilet paper roll"] = { 50.0,4 };
+	inventory["apple"] = { 1.0,15 };
+	inventory["egg"] = { 3.0,10 };
+	inventory["potato chip"] = { 4.0,8 };
+	double total = 0;
+	printInventory(inventory);
+	cout << "You can 'view', 'buy', 'exit', or 'remove item'." << endl;
+}
