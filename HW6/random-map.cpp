9ef81bcffc014a19f12855a32170b5d8f764fcaa
Harrison Moore <hbmoore115@gmail.com>
2020-04-10 18:55:54 -0800

    added include ints to random-map

--- HW6/random-map.cpp
+++ HW6/random-map.cpp
@@ -5,3 +5,38 @@
 //April 10, 2020
 //This program will..
 //
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <cmath>
+#include <stdlib.h>
+#include <random>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::random_device;
+using std::mt19937;
+
+
+int RandomBetweenU(
+    const int& first,
+    const int& last,
+    random_device& r);
+
+int RandomBetweenN(
+    const int& first,
+    const int& last,
+    const int& mean,
+    mt19937& e2);
+
+int RandomBetween(
+    const int& first,
+    const int& last);
+
+void PrintDistribution(
+    const map<int, int>& numbers,
+    const int& star);
+
