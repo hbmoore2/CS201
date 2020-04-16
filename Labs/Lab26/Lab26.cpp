d0cb7731454860837c873149bad195451ad90725
Harrison Moore <hbmoore115@gmail.com>
2020-04-15 21:14:56 -0800

    Lab26 file added

---
+++ Labs/Lab26/Lab26.cpp
@@ -0,0 +1,56 @@
+//
+//Lab26.cpp
+//Harrison Moore
+//CS201
+//April 15, 2020
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <vector>
+#include <string>
+
+#include "lambdas.h"
+
+int main() {
+
+	// TODO: REQUIRED
+	//       Create a vector<pair<size_t, string>> to enumerate
+	//       each string in the WIKIPEDIA_CPP string vector.
+
+
+
+	// TODO: REQUIRED
+	//       Use std::sort with a comparison lambda function that sorts
+	//       the vector pairs with the first member in descending order.
+
+
+
+	// TODO: REQUIRED
+	//       Use printVectorPairs() to print the vector
+
+
+
+	// TODO: ADDITIONAL 1
+	//       Use std::sort with a comparison lambda function that sorts
+	//       the vector pairs with the second member in ascending order.
+
+
+
+	// TODO: ADDITIONAL 1
+	//       Use printVectorPairs() to print the vector
+
+
+
+	// TODO: ADDITIONAL 2
+	//       Use std::find_if with a comparison lambda function that prints
+	//       the vector pairs where the pair's second.length() > 15.
+	//       Print the vector as described in the lab.
+
+
+
+	return 0;
+}
