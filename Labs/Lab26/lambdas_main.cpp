835d05fea2c852c8e94966fe04df2541a6ce86c8
Harrison Moore <hbmoore115@gmail.com>
2020-04-15 21:11:26 -0800

    added lambdas main and header file to Lab26

--- Labs/Lab26/lambdas_main.cpp
+++ Labs/Lab26/lambdas_main.cpp
@@ -2,5 +2,41 @@
 //lambdas_main.cpp
 //Harrison Moore
 //CS201
-//April 17, 2020
+//April 15, 2020
 //
+
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <vector>
+#include <string>
+
+#include "lambdas.h"
+
+int main()
+{
+	#include <cstdlib>
+	#include <iostream>
+	#include <utility>
+	#include <vector>
+	#include <string>
+	#include <sstream>
+	#include <iomanip>
+
+	std::string printVectorPairs(
+		std::vector<std::pair<std::size_t, std::string>>::iterator iter1,
+		std::vector<std::pair<std::size_t, std::string>>::iterator iter2) {
+
+		std::ostringstream output;
+		int count = 1;
+		while (iter1 != iter2)
+		{
+			output << std::setw(5) << std::left << count << "{ " << iter1->first
+				<< ", " << iter1->second << " }" << std::endl;
+			++iter1;
+			++count;
+		}
+		return output.str();
+}
---
+++ Labs/Lab28/L28.cpp
@@ -0,0 +1,6 @@
+//
+//L28.cpp
+//Harrison Moore
+//CS201
+//April 17, 2020
+//
