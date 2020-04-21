f402182a18d1be4e465bdac53d9f5ad2e3666126
Harrison Moore <hbmoore115@gmail.com>
2020-04-20 21:00:56 -0800

    included image3 header to asciiart file

--- HW7/asciiart.cpp
+++ HW7/asciiart.cpp
@@ -3,4 +3,14 @@
 //Harrison Moore
 //CS201
 //April 17, 2020
+//This program will output an ASCII art from a PPM file.
 //
+
+#include "Image3.h"
+
+struct RGB
+{
+	int R = 0;
+	int G = 0;
+	int B = 0;
+};
