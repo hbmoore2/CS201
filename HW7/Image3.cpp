71e7bc0a92e1ad9f817af8ce3c702cad75ed4949
Harrison Moore <hbmoore115@gmail.com>
2020-04-20 23:09:06 -0800

    added output message to let the user know what to do

--- HW7/Image3.cpp
+++ HW7/Image3.cpp
@@ -9,7 +9,8 @@
 #include <fstream>
 
 // Image3 Constructor
-Image3::Image3(unsigned width, unsigned height) {
+Image3::Image3(unsigned width, unsigned height)
+{
 	// TODO: resize the pixels array
 	// TODO: initialize the internal w and h members
 	w = width;
@@ -19,7 +20,8 @@ Image3::Image3(unsigned width, unsigned height) {
 }
 
 // Return a pixel from the image
-const Color3& Image3::getPixel(unsigned x, unsigned y) const {
+const Color3& Image3::getPixel(unsigned x, unsigned y) const
+{
 	// TODO: Add error handling for unknown pixels
 	// TERRIBLE OPTION 1: throw
 	// BETTER OPTION 2: return a color
@@ -33,12 +35,14 @@ const Color3& Image3::getPixel(unsigned x, unsigned y) const {
 	return pixels[y * w + x];
 }
 
-void Image3::setPixel(unsigned x, unsigned y, const Color3& color) {
+void Image3::setPixel(unsigned x, unsigned y, const Color3& color)
+{
 	// TODO: Set the pixel to the new color
 	pixels[y * w + x] = color;
 }
 
-bool Image3::savePPM(const std::string& path) const {
+bool Image3::savePPM(const std::string& path) const
+{
 	// TODO: Save the image to the disk
 	// REQUIREMENT: Use the STREAM operators for the file contents
 	std::ofstream fout(path);
@@ -50,7 +54,8 @@ bool Image3::savePPM(const std::string& path) const {
 	return false;
 }
 
-bool Image3::loadPPM(const std::string& path) {
+bool Image3::loadPPM(const std::string& path)
+{
 	// TODO: Load an image from the disk
 	// REQUIREMENT: Use the STREAM operators for the file contents
 	std::ifstream fin(path);
@@ -66,12 +71,15 @@ bool Image3::loadPPM(const std::string& path) {
 	return true;
 }
 
-void Image3::printASCII(std::ostream& ostr) const {
+void Image3::printASCII(std::ostream& ostr) const
+{
 	// TODO: Print an ASCII version of this image
 
-	for (std::size_t i = 0; i < pixels.size(); i++) {
+	for (std::size_t i = 0; i < pixels.size(); i++)
+	{
 		ostr << pixels[i].asciiValue();
-		if (i != 0 && (i + 1) % w == 0) {
+		if (i != 0 && (i + 1) % w == 0)
+		{
 			ostr << '\n';
 		}
 	}
@@ -79,17 +87,20 @@ void Image3::printASCII(std::ostream& ostr) const {
 
 // STREAM OPERATORS for IMAGE3 class
 
-std::ostream& operator<<(std::ostream& ostr, const Image3& image) {
+std::ostream& operator<<(std::ostream& ostr, const Image3& image)
+{
 	// TODO: Write out PPM image format to stream
 	// ASSUME FORMAT WILL BE GOOD
 	ostr << "P3\n" << image.w << ' ' << image.h << "\n255";
-	for (Color3 px : image.pixels) {
+	for (Color3 px : image.pixels)
+	{
 		ostr << '\n' << px;
 	}
 	return ostr;
 }
 
-std::istream& operator>>(std::istream& istr, Image3& image) {
+std::istream& operator>>(std::istream& istr, Image3& image)
+{
 	// TODO: Read in PPM image format from stream
 	// MAKE SURE FORMAT IS GOOD!!!
 	std::string line;
@@ -112,26 +123,31 @@ std::istream& operator>>(std::istream& istr, Image3& image) {
 		}
 		if (line[0] == '#' || line == "") continue;
 		std::istringstream str(line);
-		while (str) {
+		while (str)
+		{
 			int value;
 			str >> value;
 			if (!str) break;
-			if (!widthSet) {
+			if (!widthSet)
+			{
 				widthSet = true;
 				image.w = value;
 				continue;
 			}
-			if (!heightSet) {
+			if (!heightSet)
+			{
 				heightSet = true;
 				image.h = value;
 				continue;
 			}
-			if (!colorspaceSet) {
+			if (!colorspaceSet)
+			{
 				colorspaceSet = true;
 				continue;
 			}
 			rgb.push_back(value);
-			if (rgb.size() == 3) {
+			if (rgb.size() == 3)
+			{
 				image.pixels.push_back(Color3(rgb[0], rgb[1], rgb[2]));
 				rgb.clear();
 			}
--- HW7/caesar-cypher/caesar-cypher.cpp
+++ HW7/caesar-cypher/caesar-cypher.cpp
@@ -4,3 +4,36 @@
 //April 20, 2020
 //This program will...
 //
+
+#include <string>
+using std::string;
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <sstream>
+using std::istringstream;
+#include <vector>
+using std::vector;
+
+unsigned StringToTokenWS(const string& user_string, vector<string>& tokens);
+
+int main()
+{
+	string user_string;
+	string cypher_shift_str;
+	int cypher_shift_val;
+	cout << "Caesar Cypher" << endl;
+	cout << endl;
+	while (true)
+	{
+		vector<string> tokens;
+		// Takes users line
+		cout << "Enter a message to cypher: ";
+		std::getline(cin, user_string);
+		if (user_string.size() == 0) // Checks if line is blank
+			break;
+		else
+			StringToTokenWS(user_string, tokens);
+	}
+}
