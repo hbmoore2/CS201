95c1edb0252dff9d9bf91d1abbee5f532dbe6228
Harrison Moore <hbmoore115@gmail.com>
2020-04-22 20:13:20 -0800

    finished lab34

--- Labs/Lab34/Lab34.cpp
+++ Labs/Lab34/Lab34.cpp
@@ -6,26 +6,41 @@
 //
 
 #include <iostream>
+#include <iomanip>
 using std::cout;
 using std::endl;
 using std::cin;
-
+using std::setw;
+using std::setfill;
 
 // Class Time
 // Holds a time of day: hours, minutes, seconds. Can print its stored
 // time in a nice format.
-class Time {
+class Time
+{
 
 public:
     // ***** Time: constructors *****
 
         // TODO: Put something here!
+    Time() :
+        _hr{ 12 },
+        _min{ 0 },
+        _sec{ 0 }
+    {} // 12:25:35
+
+    Time(int a, int b, int c) :
+        _hr{ a },
+        _min{ b },
+        _sec{ c }
+    {} // a:b:c
 
     // ***** Time: general public member functions *****
 
     void print() const
     {
-        cout << "SOMETHING NEEDS TO GO HERE";  // DUMMY
+        //_hr = 2; // '_hr' cannot be modified because it is being accessed through a const object
+        cout << setw(2) << setfill('0') << _hr << ":" << setw(2) << setfill('0') << _min << ":" << setw(2) << setfill('0') << _sec;  // DUMMY
         // TODO: Write this!
     }
 
@@ -47,8 +62,14 @@ int main()
 
     // Make a Time object; print it
     Time t1;
+    Time t2(3, 48, 32);
     cout << "Time #1: [";
     t1.print();
+    cout << "]" << endl;
+    cout << endl;
+
+    cout << "Time #2: [";
+    t2.print();
     cout << "]" << endl;
     cout << endl;
 
