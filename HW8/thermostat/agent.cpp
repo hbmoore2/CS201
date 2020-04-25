8ea1234245f8011f50ede58ae7196eec5b5905ea
Harrison Moore <hbmoore115@gmail.com>
2020-04-24 21:40:26 -0800

    agent gets temperature from environment

--- HW8/thermostat/agent.cpp
+++ HW8/thermostat/agent.cpp
@@ -4,3 +4,33 @@
 //CS 201
 //April 24, 2020
 //
+
+#include "thermostat.h"
+
+		// Gets the temperature from environment
+void Agent::perceive(Environment& location)
+{
+	_current_temp = location.get_temp();
+}
+
+	// If the temperature is less than the upper bound, then 
+	// heater is on until it hits below lower range
+void Agent::think()
+{
+	if (_current_temp < _lower)
+		_act = true;
+	if (_current_temp > _upper)
+		_act = false;
+}
+
+		// Gets the upper and lower bounds from simulator
+void Agent::get_range(int lower, int upper)
+{
+	_lower = lower;
+	_upper = upper;
+}
+
+void Agent::act(Environment& location)
+{
+	location._heater = _act;
+}
--- HW8/thermostat/agent.h
+++ HW8/thermostat/agent.h
@@ -4,3 +4,30 @@
 //CS 201
 //April 24, 2020
 //
+
+#ifndef Agent_H
+#define Agent_ccp
+
+#include "thermostat.h"
+
+class Agent
+{
+public:
+	Agent() : _act{ false }, _current_temp{ 0 }, _lower{ 0 }, _upper{ 5 }  {};
+	void perceive(Environment& location); // Gets the temperature from environment
+	void think();
+	void get_range(int lower, int upper);
+
+	void act(Environment& location);
+
+
+
+private:
+	int _current_temp;
+	int _lower;
+	int _upper;
+	bool _act;		 // Tells environment to turn on or off
+};
+
+#define Agent_H
+#endif
