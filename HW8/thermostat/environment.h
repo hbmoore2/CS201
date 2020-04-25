c9713c62ab78a56df415ae5755e2e393a78dfb8e
Harrison Moore <hbmoore115@gmail.com>
2020-04-24 21:36:33 -0800

    environment header file done

--- HW8/thermostat/environment.h
+++ HW8/thermostat/environment.h
@@ -4,3 +4,24 @@
 //CS 201
 //April 24, 2020
 //
+
+#ifndef environment_H
+#define environment_ccp
+
+#include "thermostat.h"
+
+class Environment
+{
+public:
+	Environment() : _heater{ false }, _temperature{ 50 } {}
+	void itteration();		// Changes temperature
+	int get_temp();			// Gives temperature
+
+	bool _heater;
+private:
+	int _temperature;
+
+};
+
+#define environment_H
+#endif
