447a99c709fc65c2aac85ae06e88f3d5165db049
Harrison Moore <hbmoore115@gmail.com>
2020-04-24 21:29:58 -0800

    added to thermostat and header file

--- HW8/thermostat/thermostat.cpp
+++ HW8/thermostat/thermostat.cpp
@@ -5,3 +5,16 @@
 //April 24, 2020
 //This program will be used as a thermostat.
 //
+
+#include "thermostat.h"
+
+int main()
+{
+	Simulator Neo;
+	Neo.run();
+
+	// Stops the console from closing.
+	cout << "Program end" << endl;
+	int q;
+	cin >> q;
+}
--- HW8/thermostat/thermostat.h
+++ HW8/thermostat/thermostat.h
@@ -5,3 +5,44 @@
 //April 24, 2020
 //This program will declare all other headers for project.
 //
+
+#ifndef thermostat_H
+#define thermostat_H
+
+#include "Simulator.h"
+#include "Environment.h"
+#include "Agent.h"
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <vector>
+using std::vector;
+#include <cstdlib>
+#include <ctime>
+#include <Windows.h>
+#include <random>
+using std::random_device;
+#include <string>
+using std::string;
+#include <iomanip>
+using std::setw;
+using std::right;
+#include <sstream>
+using std::istringstream;
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+#include <map>
+using std::map;
+#include <utility>
+using std::pair;
+#include <ios>
+using std::left;
+
+using std::size_t;
+
+#define thermostat_H
+#endif
