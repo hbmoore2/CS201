da24d847fc05d30b890339fff2273b3646cba5d1
Harrison Moore <hbmoore115@gmail.com>
2020-04-24 21:43:37 -0800

    header for simulator finished

--- HW8/thermostat/simulator.h
+++ HW8/thermostat/simulator.h
@@ -4,3 +4,26 @@
 //CS 201
 //April 24, 2020
 //
+
+#ifndef Simulator_H
+#define Simulator_ccp
+
+#include "thermostat.h"
+
+class Simulator
+{
+public:
+	Simulator() : _lower{ 0 }, _upper{ 5 }, _proceed{ true }, _iter_count{ 0 } {};
+	void run();			// Runs simulation 
+	void askOwner();	// Gets the temperature from the user
+
+	int _lower;
+	int _upper;
+
+private:
+	bool _proceed;
+	int _iter_count;
+};
+
+#define Simulator_H
+#endif
